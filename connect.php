9735602a22fe005b55364f64e780624c9f90d887
Chhotu602 <141182144+Chhotu602@users.noreply.github.com>
2023-08-02 14:32:25 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,19 +1,1 @@
-<?php
-$firstname=$_post['$firstname'];
-$number =$_post['$number'];
-$email=$_post['$email'];
-$opinion=$_post['$opinion'];
-$conn=new mysqli('localhost','root','test');
-if($conn->connect_error){
-    die('Connection Failed :'.$conn->connect_error);
 
-}
-else{
-    $stmt=$conn->prepare("insert into registration (firstname,number,email,opinion)values(?,?,?,?)");
-    $stmt->bind_param("siss",$firstname,$number,$email,$opinion);
-    $stmt->execute();
-    echo "Sign Up Successfull...";
-    $stmt->close();
-    $conn->close();
-}
-?>
