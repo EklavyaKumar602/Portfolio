dabd24b4f7f902bcd928e10881967d7ba164e30c
Chhotu602 <141182144+Chhotu602@users.noreply.github.com>
2023-09-22 23:49:48 +0530

    Update connect.php
    uh

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['submit'])
+if(isset($_POST['submit'])){
 $name=$_POST['name'];
 $mob=$_POST['number'];
 $email=$_POST['email'];
@@ -8,6 +8,7 @@ $to="cs901934@gmail.com";
 $subject="Mail From Website";
 $txt="Name = ".$name ."\r\n Mob =". $mob ."\r\n Email =" . $email ."\r\n Message =".$message;
 $headers = "From : Your Portfolio ";
+}
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
