de0ea3aa1d675a43308c8ece512e5356b97d48f1
Chhotu602 <141182144+Chhotu602@users.noreply.github.com>
2023-09-22 21:33:40 +0530

    Update connect.php
    now connent my php

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$name=$_POST['name'];
+$mob=$_POST['number'];
+$email=$_POST['email'];
+$message=$_POST['opinion'];
+$to="cs901934@gmail.com";
+$subject="Mail From Website";
+$txt="Name = ".$name ."\r\n Mob =". $mob ."\r\n Email =" . $email ."\r\n Message =".$message;
+$headers = "From : Your Portfolio ";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+
+header("Location:thankyou.");
+?>
