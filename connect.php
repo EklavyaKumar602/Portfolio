fba98a15f64bc0d5caa86d0a309fbcb31e94e914
Chhotu602 <141182144+Chhotu602@users.noreply.github.com>
2023-09-23 20:11:28 +0530

    Update connect.php
    fh

--- connect.php
+++ connect.php
@@ -1,4 +1,23 @@
 <?php
+require 'vendor/autoload.php';
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->isSMTP();
+$mail->Host = 'smtp.example.com'; // Your SMTP server
+$mail->SMTPAuth = true;
+$mail->Username = 'your_username';
+$mail->Password = 'your_password';
+$mail->SMTPSecure = 'tls'; // Use 'tls' or 'ssl' depending on your server settings
+$mail->Port = 587; // Use 587 for TLS or 465 for SSL
+
+// Set sender and recipient information
+$mail->setFrom('yourname@example.com', 'Your Name');
+$mail->addAddress('cs901934@gmail.com', 'Recipient Name');
+
+// Set email content
+$mail->Subject = 'Hello, World!';
+$mail->Body = 'This is a test email sent from PHPMailer.';
+
+// Send the email
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name=$_POST['uname'];
     $mob=$_POST['number'];
