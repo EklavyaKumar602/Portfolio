eba70155ab87438da1d6bcce68e22814d114fa7d
Chhotu602 <141182144+Chhotu602@users.noreply.github.com>
2023-09-23 11:37:52 +0530

    Update connect.php
    ht

--- connect.php
+++ connect.php
@@ -1,13 +1,13 @@
 <?php
 if(isset($_POST['esubmit'])){
-$name=$_POST['name'];
+$name=$_POST['uname'];
 $mob=$_POST['number'];
 $email=$_POST['email'];
 $message=$_POST['opinion'];
 $to="cs901934@gmail.com";
 $subject="Mail From Website";
 $txt="Name = ".$name ."\r\n Mob =". $mob ."\r\n Email =" . $email ."\r\n Message =".$message;
-$headers = "From : Your Portfolio : $from";
+$headers = "From : Your Portfolio :";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
