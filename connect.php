1ad1b418d4bb167ad03597bef3a5bd8ed2e4e15f
Chhotu602 <141182144+Chhotu602@users.noreply.github.com>
2023-09-23 20:04:14 +0530

    Update connect.php
    grat

--- connect.php
+++ connect.php
@@ -1,19 +1,21 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-$name=$_POST['uname'];
-$mob=$_POST['number'];
-$email=$_POST['email'];
-$message=$_POST['opinion'];
-$to="cs901934@gmail.com";
-$subject="Mail From Website";
-$txt="Name = ".$name ."\r\n Mob =". $mob ."\r\n Email =" . $email ."\r\n Message =".$message;
-$headers = "From : Your Portfolio : $form";
-if(mail($to,$subject,$txt,$headers)){
-    echo"Mail Sent";
-}
-else{
-    echo "Oops! Something went wrong";
-}
-echo"Thanks for Contacting Us.";
+    $name=$_POST['uname'];
+    $mob=$_POST['number'];
+    $email=$_POST['email'];
+    $message=$_POST['opinion'];
+    
+    $to="cs901934@gmail.com";
+    $subject="Mail From Website";
+    $txt="Name = ".$name ."\r\n Mob =". $mob ."\r\n Email =" . $email ."\r\n Message =".$message;
+    $headers = "From : Your Portfolio : $form";
+    if(mail($to,$subject,$txt,$headers)){
+        echo"Mail Sent";
+    }
+    else
+    {
+        echo "Oops! Something went wrong";
+    }
+    echo"Thanks for Contacting Us.";
 }
 ?>
