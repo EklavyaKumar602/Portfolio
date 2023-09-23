51520c8d9ff424f6c23204d27fad059fd61c9d12
Chhotu602 <141182144+Chhotu602@users.noreply.github.com>
2023-09-23 07:19:48 +0530

    Update connect.php
    hj

--- connect.php
+++ connect.php
@@ -1,9 +1,10 @@
 <?php
-if(isset($_GET['esubmit'])){
+if(isset($_POST['esubmit'])){
 $name=$_REQUEST['name'];
 $mob=$_REQUEST['number'];
 $email=$_REQUEST['email'];
 $message=$_REQUEST['opinion'];
+$con=mysqli_connect('cs901934@gmail.com');
 $to="cs901934@gmail.com";
 $subject="Mail From Website";
 $txt="Name = ".$name ."\r\n Mob =". $mob ."\r\n Email =" . $email ."\r\n Message =".$message;
